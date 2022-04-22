75406e14f4e3a9e816618fd4d75b1dd97a90b66d
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2022-04-22 13:19:50 -0400

    initial commit

---
+++ Exo1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    //Afficher dans la console : Hello World !
+    printf("Hello world");
+
+    return 0;
+}
---
+++ Exo2/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello quentin\n");
+    return 0;
+}
